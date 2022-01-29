2dc7bcb4517b55ae51ce2f35af2ae9041b6a55f0
Naim <naimzian.dev@gmail.com>
2022-01-29 14:16:45 +0100

    Adding bases for the projetcts + Components

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD_ieKT2namWu5-DqUevjWcKkzzf29eNes",
+  authDomain: "netflix-clone-8512c.firebaseapp.com",
+  projectId: "netflix-clone-8512c",
+  storageBucket: "netflix-clone-8512c.appspot.com",
+  messagingSenderId: "79270339342",
+  appId: "1:79270339342:web:73c5fe685ccd16f28eed5f",
+};
